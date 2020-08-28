18f5b2b5c5037367b304407e26fcef5700533a23
Lenovo <priyanshi.jaiswal_cs19@gla.ac.in>
2020-08-28 10:29:02 +0530

    add scanner class in FrontDesk.java; and taken the value for book

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -8,8 +8,31 @@ package execution;
 
 import definitions.Book;
 
+import java.util.Scanner;
+
 public class FrontDesk {
     public static void main(String[] args) {
         Book book = new Book();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the Book Name: ");
+        String bookName = scanner.nextLine();
+        book.setBookName(bookName);
+
+        System.out.println("Enter the Author Name: ");
+        String authorName = scanner.nextLine();
+        book.setAuthorName(authorName);
+
+        System.out.println("Enter the ISBN Number: ");
+        String isbnNumber = scanner.nextLine();
+        book.setIsbnNumber(isbnNumber);
+
+        scanner.close();
+
+        System.out.println(
+                "Book Name: " + book.getBookName() + ", " +
+                        "Author Name: " + book.getAuthorName() + ", " +
+                        "ISBN Number: " + book.getIsbnNumber() + "."
+        );
     }
 }
