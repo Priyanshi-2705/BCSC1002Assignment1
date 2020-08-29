9ae4be117e0e31f523b3521bc9adad0d7c4cd02a
Lenovo <priyanshi.jaiswal_cs19@gla.ac.in>
2020-08-29 23:45:19 +0530

    add constants to denote menu option

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -9,6 +9,11 @@ package execution;
 import java.util.Scanner;
 
 public class FrontDesk {
+    public static final int ISSUE_A_NEW_BOOK_FOR_ME = 1;
+    public static final int RETURN_A_PREVIOUSLY_ISSUES_BOOK_FOR_ME = 2;
+    public static final int SHOW_ME_ALL_MY_ISSUES_BOOK = 3;
+    public static final int EXIT = 4;
+
     public static void main(String[] args) {
         System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
         System.out.println("How may I help you today?");
