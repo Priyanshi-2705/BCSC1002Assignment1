9f6d68fed18162e640bf06f918e9b84cbce1962d
Lenovo <priyanshi.jaiswal_cs19@gla.ac.in>
2020-08-30 12:57:30 +0530

    create construtor, getter and setter for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -13,5 +13,75 @@ public class Student {
     private String fullName;
     private long uniRollNumber;
     private int bookIssued;
-    private Book[] issued;
+    private Book[] issuedBookName;
+
+    public Student(String firstName, String middleName, String lastName, String fullName, long uniRollNumber, int bookIssued) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.fullName = firstName + middleName + lastName;
+        this.uniRollNumber = uniRollNumber;
+        this.bookIssued = bookIssued;
+        this.issuedBookName = new Book[bookIssued];
+        for (int i = 0; i < issuedBookName.length; i++) {
+            issuedBookName[i] = new Book("Book" + (i + 1));
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public long getUniRollNumber() {
+        return uniRollNumber;
+    }
+
+    public void setUniRollNumber(long uniRollNumber) {
+        this.uniRollNumber = uniRollNumber;
+    }
+
+    public int getBookIssued() {
+        return bookIssued;
+    }
+
+    public void setBookIssued(int bookIssued) {
+        this.bookIssued = bookIssued;
+    }
+
+    public Book[] getIssued() {
+        return issuedBookName.clone();
+    }
+
+    public void setIssued(Book[] issued) {
+        this.issuedBookName = issued;
+    }
+
 }
